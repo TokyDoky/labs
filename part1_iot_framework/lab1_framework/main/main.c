25c61eb908b7d41bea44d308b9e2de17b46da487
TokyDoky <nicovougny@hotmail.fr>
2022-09-29 09:41:31 +0200

    2commitvsc

---
+++ part1_iot_framework/lab1_framework/main/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * @brief Starting point function
+ * 
+ */
+
+void app_main(void) {
+
+	printf("Oh putain c'est venretchiiiiiiiiiiiiii !\n");
+	
+}
